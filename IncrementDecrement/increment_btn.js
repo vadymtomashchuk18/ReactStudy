7e702733ad8dbba6ea91466af68fd3b23a9b30e3
vadymtomashchuk18 <vadym.tomashchuk@ukma.edu.ua>
2018-06-11 17:19:00 +0300

    Optimize handling

--- IncrementDecrement/increment_btn.js
+++ IncrementDecrement/increment_btn.js
@@ -1,17 +1,13 @@
 // Write JavaScript here and press Ctrl+Enter to execute
 class Button extends React.Component {
   
-  // handleClick = () => {
-  //  this.setState((prevState) => {
-  //    return {
-  //      counter: prevState.counter + 1
-  //     };
-  //   });
-  // }
+  handleClick = () => {
+   this.props.onClickFunction(this.props.value);
+  }
   
   render () {
     return (
-      <button onClick={() => this.props.onClickFunction(this.props.value)}>{this.props.label}{this.props.value}</button>
+      <button onClick={this.handleClick}>{this.props.label}{this.props.value}</button>
     );
   }
 }
@@ -56,12 +52,12 @@ class App extends React.Component {
         <Button label="-" value={1} onClickFunction={this.decrementCounter} />
         <Button label="-" value={5} onClickFunction={this.decrementCounter} />
         <Button label="-" value={10} onClickFunction={this.decrementCounter} />
-        <Button label="-" value={50} onClickFunction={this.decrementCounter} />
+        <Button label="-" value={100} onClickFunction={this.decrementCounter} />
         <Result counter={this.state.counter}/>
         <Button label="+" value={1} onClickFunction={this.incrementCounter} />
         <Button label="+" value={5} onClickFunction={this.incrementCounter} />
         <Button label="+" value={10} onClickFunction={this.incrementCounter} />
-        <Button label="+" value={50} onClickFunction={this.incrementCounter} />
+        <Button label="+" value={100} onClickFunction={this.incrementCounter} />
         <p></p>
         <Button label="Reset" onClickFunction={this.resetCounter} />
       </div>  
